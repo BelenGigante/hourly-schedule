8ca48ed12ee701a44b6b812bd3bedc06f9873c77
BelenGigante <vinn_a@hotmail.com>
2022-12-08 23:18:04 -0500

    deactivating past textboxes and getitem

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var currDate = $('#currentDay');
+var textBox = localStorage.getItem($(".time-block").attr("id"));
 
 currDate.text(new Date());
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
@@ -32,9 +33,7 @@ $(function (){
     }
     
   });
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+  // TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements. HINT: How can the id attribute of each time-block be used to do this?
+  
   // TODO: Add code to display the current date in the header of the page.
 });
