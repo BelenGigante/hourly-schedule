2cd9dd7bf8c0d40223a5a8d5907df3bede09b8c8
BelenGigante <vinn_a@hotmail.com>
2022-12-08 23:46:37 -0500

    deleted instruccions

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,28 +5,29 @@ var textBox = localStorage.getItem($(".time-block").attr("id"));
 var plan = "";
 
 currDate.text(new Date());
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 $(function (){
 
   var currentHour = dayjs().hour();
   console.log(currentHour);
 
   
-
-  // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage. HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
   $(".time-block").on("click",".saveBtn",function(){
       plan = $(this).prev().val();
      hourx = $(this).parent().attr("id");
     localStorage.setItem(hourx,plan);
   
   });
-  // TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?
+  
+  $(".time-block").find(description).val(textBox);
+ 
+
   $(".time-block").each(function(){
+
     var hour = parseInt($(this).attr("id").split("-")[1]);
     if (hour < currentHour){
       $(this).addClass("past");
+
       $(".time-block").attr("disabled",true);
 
     }else if(hour > currentHour){
@@ -36,8 +37,6 @@ $(function (){
     }
     
   });
-  // TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements. HINT: How can the id attribute of each time-block be used to do this?
 
-  $(".time-block").find(description).val(textBox);
-  // TODO: Add code to display the current date in the header of the page.
+
 });
